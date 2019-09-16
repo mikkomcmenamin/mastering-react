f10b4d45f87c0dd682cb3b21acfd1811b6d8efb5
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2019-09-16 13:09:31 +0300

    Add counter-app

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+
+import Teacher, { promote } from "./teacher";
 
 const element = (<h1>Hello World</h1>);
 
 ReactDOM.render(element, document.getElementById("root"));
+
 
 
 
---
+++ react-app/src/teacher.js
@@ -0,0 +1,15 @@
+import { Person } from "./person";
+
+export function promote () {
+
+}
+
+export class Teacher extends Person{
+    constructor(name, degree){
+        super(name);
+        this.degree = degree;
+    }
+    teach(){
+        console.log("teach");
+    }
+}
