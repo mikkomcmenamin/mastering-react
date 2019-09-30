d46d3d6c69df2052ed4c9f129a791e1e05623539
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2019-09-30 16:00:31 +0300

    Single source of truth for counters

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,6 +11,22 @@ class Counters extends Component {
     ]
   };
 
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({counters});
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     //because key and value are the same, the counters: counters can be simplified to counters
@@ -20,10 +36,15 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2">Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
