b80c7f1489989706485b00f863c92d21c9d367cf
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2019-10-28 12:22:50 +0200

    Finish Pagination and start List group

---
+++ vidly/src/components/common/ListGroup.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ListGroup extends Component {
+    render() {
+        return (
+            <div className="list-group">
+                <a href="#" className="list-group-item list-group-item-action">All Genres</a>
+
+            </div>
+        );
+    }
+}
+
+export default ListGroup;
