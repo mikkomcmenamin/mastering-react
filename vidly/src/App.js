4ba9d7ffbe92d68d0dcd939e0acb848bf57707cd
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2020-01-01 16:38:25 +0200

    Finish routing features

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/Movies";
+import Navbar from "./components/navbar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <Navbar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
