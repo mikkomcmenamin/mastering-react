f8d40dafe23e3be953803f88746722d34664f72a
Mikko McMenamin <mikko.mcmenamin@intopalo.com>
2020-01-19 11:23:01 +0200

    Updates

---
+++ precache-manifest.8cc1a378c17e5dd2d9a4de8a5e2d80eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df65289c355ca58490de36be9914be91",
+    "url": "/mastering-react/index.html"
+  },
+  {
+    "revision": "5c70bc0919aed88aadf1",
+    "url": "/mastering-react/static/css/2.8a6da4b6.chunk.css"
+  },
+  {
+    "revision": "9f1651ed5ab7c4ac4c22",
+    "url": "/mastering-react/static/css/main.e527cb9f.chunk.css"
+  },
+  {
+    "revision": "5c70bc0919aed88aadf1",
+    "url": "/mastering-react/static/js/2.7fd570f2.chunk.js"
+  },
+  {
+    "revision": "9f1651ed5ab7c4ac4c22",
+    "url": "/mastering-react/static/js/main.6c0f199f.chunk.js"
+  },
+  {
+    "revision": "aea7dec2ac5386c32501",
+    "url": "/mastering-react/static/js/runtime~main.af4b1537.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mastering-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mastering-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mastering-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mastering-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mastering-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
